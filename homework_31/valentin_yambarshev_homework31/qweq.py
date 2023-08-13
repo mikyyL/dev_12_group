9e43eecf1666062e9a4e769425be9f5c6c077b80
artem <mikyedit@gmail.com>
2023-08-13 10:13:56 +0300

    Revert "homework31"
    This reverts commit 2691222e11e711fb7dbf20663fc185ba754e71cd.

--- homework_31/valentin_yambarshev_homework31/qweq.py
+++
@@ -1,8 +0,0 @@
-def requst_post():
-    data = {
-        'login': 'TimeFF84a',
-        'password': 'HondaPrelude9298',
-        'authenticity_token': ''
-    }
-    return data['login']
-print(requst_post())
